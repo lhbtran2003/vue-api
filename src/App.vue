0a5cdaef910df1f1bc0e5a43cc350cf7ce672aa4
lhbtran2003 <lhbtran2003@gmail.com>
2024-11-05 08:17:16 +0700

    commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import ListProduct from "./components/ListProduct.vue";
+</script>
+
+<template>
+  <ListProduct/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ListProduct.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import axios from "axios";
+import {createLogger} from "vite";
+
+const getAllProducts = async () => {
+  const res = await axios.get("http://localhost:3001/post");
+  return res.data;
+}
+
+// console.log(getAllProducts().then(res => res.data));
+getAllProducts().then(res =>  console.log(res));
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
